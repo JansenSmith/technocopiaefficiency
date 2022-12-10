b52e5c8cf1e28be8c3ce0959cc5483a48a7ac9d2
Jansen M Smith <jansen@bstarengineering.com>
2022-12-10 16:14:43 -0500

    path to coverage in sq meters

--- FromSunlight.groovy
+++ FromSunlight.groovy
@@ -1,8 +1,8 @@
 
-double coverage_sqft = 10000 
+double coverage_ft2 = 10000 
 double feet_per_meter = 3.28084
-double coverage_sqm = coverage_sqft / feet_per_meter^2
+double coverage_m2 = coverage_ft2 / Math.pow(feet_per_meter,2)
 double solar_insolation_per_m2 = 4.5 // kilowatt-hours per square meter for an average yearly day in Massachusetts, estimated from https://www.solar-electric.com/learning-center/solar-insolation-maps.html/
 double solar_panel_efficiency = 0.15
 
-print(coverage_sqm)
+print(coverage_m2)
