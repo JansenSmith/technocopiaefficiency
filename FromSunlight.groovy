78d435fb34e65b66595621e2808a386285d70815
Jansen M Smith <jansen@bstarengineering.com>
2022-12-10 15:30:56 -0500

    Ignore the project files

--- FromSunlight.groovy
+++ FromSunlight.groovy
@@ -1,1 +1,8 @@
-//Your code here
+
+double coverage_sqft = 10000 
+double feet_per_meter = 3.28084
+double coverage_sqm = coverage_sqft / feet_per_meter^2
+double solar_insolation_per_m2 = 4.5 // kilowatt-hours per square meter for an average yearly day in Massachusetts, estimated from https://www.solar-electric.com/learning-center/solar-insolation-maps.html/
+double solar_panel_efficiency = 0.15
+
+print(coverage_sqm)
