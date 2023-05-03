448389edacc72339d4ea8a16e2edb29e31d40229
JansenSmith <jansen@bstarengineering.com>
2023-05-03 15:48:29 -0400

    clarified commenting

--- FromSunlight.groovy
+++ FromSunlight.groovy
@@ -10,7 +10,8 @@ double kilowatthours_per_day = coverage_m2 * solar_insolation_per_m2 * solar_pan
 def formatted_kwh = String.format("%.2f", kilowatthours_per_day)
 println("kW*hr/day = $formatted_kwh")
 
-double growing_kilowatthours_per_day = kilo
+double power_draw = 0.0				// assumed amountof power in Watts being used to run the robots et the building et al
+double growing_kilowatthours_per_day = kilowatthours_per_day - power_draw		//	energy per day used for grow light LEDs
 
 double watt_per_sqft_grow_area = 32 // see Grow Lights: How Many Watts Per Square Foot is Needed? https://trimleaf.com/blogs/articles/grow-lights-how-many-watts-per-plant-square-foot-is-needed#:~:text=Thirty%2Dtwo%20watts%20of%20power,beyond%2040%20per%20square%20foot
 double LED_hrs_per_day = 16
